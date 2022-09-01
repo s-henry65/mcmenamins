7e58d2ac1e3067a049ea1646ce356d028466e292
Scott <scott.henry.moore@gmail.com>
2022-09-01 08:14:18 -0700

    pre-deploy update

--- mcmen_dist_project/settings.py
+++ mcmen_dist_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'some_random_default_string')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'mcmenamins-dist.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'mcmenaminsdistribution.pythonanywhere.com']
 
 
 # Application definition
