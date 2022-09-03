b3bf7401a7b900973ef084332e3b4a8e08667e60
Scott Moore <scott.henry.moore@gmail.com>
2022-09-03 00:50:23 +0000

    production tweeks

--- mcmen_dist_project/settings.py
+++ mcmen_dist_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'some_random_default_string')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG', True)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'mcmenaminsdistribution.pythonanywhere.com']
 
